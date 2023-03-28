0e8464c8ad6cbe8f1d60f08bc8dc5180f9b6e596
BCD15 <bruno69correadias@gmail.com>
2023-03-28 16:48:28 -0300

    erro

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import Card from './src/components/Card';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Card />
+      <Text style={{ fontSize: 25}}>
+      Olá Mundo
+      </Text>
+      <View style={styles.conteudo}>
+        { cachorros.map(cachorro => <Card cachorro={cachorro}/>)}
+      </View>
     </View>
   );
 }
@@ -12,10 +18,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0d0',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 20,
+  },
+  conteudo: {
+    flex: 1,
+    backgroundColor: '#ccc',
     flexDirection: 'row',
     flexWrap: 'wrap',
-  },
+    width: '100%',
+  }
+
 });
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,50 @@
-import { View, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { Image } from 'react-native';
+import App from '../../App';
+
 
 export default function Card(props) {
+  const cachorros = [
+    {
+      imgcachorro: "./img/bulldog.webp",
+      raça: 'Bulldog', 
+    },
+    {
+      imgcachorro: "./img/pastor-alemão.jpg",
+      raça: 'Pastor Alemão',
+    },
+    {
+      imgcachorro: "./img/pitbull-p.webp",
+      raça: 'Pitbull',
+    },
+    {
+      imgcachorro: "./img/shitzu.jpeg",
+      raça: 'Shitzu',
+    },
+    {
+      imgcachorro: "./img/yorkshire.jpg",
+      raça: 'Yorkshire',
+    },
+  ]
   return (
     <View style={styles.card}>
-        <Image style={styles.card} source={{ uri: props.cachorro.foto}} />
+        <Image style={styles.foto} source={{ uri: props.cachorro.imgcachorro}} />  
         <Text style={{ color: 'darkred'}}> {props.cachorro.raça} </Text>
-        <Text></Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   card: {
-    backgroundColor: 'coral',
-    padding: 20,
-    width: '100%',
-    alignContent: 'center',
-    alignItems: 'center',
+    backgroundColor: 'orange',
+    width: '40%',
+    height: 215,
+    borderRadius: 10,
+    margin: 15,
   },
-});
+  foto: {
+    resizeMode: 'stretch',
+    width: '100%',
+    height: 180,
+  }
+});     
