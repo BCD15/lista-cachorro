d94b62ab28a4530ab98c6ac2509b697babaa205e
BCD15 <bruno69correadias@gmail.com>
2023-03-29 11:06:26 -0300

    create card

--- App.js
+++ App.js
@@ -1,17 +1,47 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
 import Card from './src/components/Card';
 
 export default function App() {
 
+  const cachorros = [
+    {
+      id: 1,
+      imgcachorro: "https://img.freepik.com/fotos-gratis/jovem-bulldog-frances-marrom-brincando-isolado-na-parede-branca-do-estudio_155003-31898.jpg?w=2000",
+      raça: 'Bulldog', 
+    },
+    {
+      id: 2,
+      imgcachorro: "https://www.petlove.com.br/images/breeds/193103/profile/original/pastor_alemao-p.jpg?1532539270",
+      raça: 'Pastor Alemão',
+    },
+    {
+      id: 3,
+      imgcachorro: "https://webcachorros.com.br/wp-content/uploads/2021/03/happy-brown-pitbull-dog-with-white-background_297978-1.jpg",
+      raça: 'Pitbull',
+    },
+    {
+      id: 4,
+      imgcachorro: "https://uploads.metropoles.com/wp-content/uploads/2019/11/12180309/shihtzu.jpg",
+      raça: 'Shitzu',
+    },
+    {
+      id: 5,
+      imgcachorro: "https://i0.statig.com.br/bancodeimagens/imgalta/0c/bj/hf/0cbjhfoofdbpjv3fgfbdr7sgt.jpg",
+      raça: 'Yorkshire',
+    },
+  ]
+
   return (
-    <View style={styles.container}>
-      <Text style={{ fontSize: 25}}>
-      Olá Mundo
-      </Text>
-      <View style={styles.conteudo}>
-        { cachorros.map(cachorro => <Card cachorro={cachorro}/>)}
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={{ fontSize: 25}}>
+        Olá Mundo
+        </Text>
+        <View style={styles.conteudo}>
+        { cachorros.map(cachorro => <Card cachorro={cachorro} key={cachorros.id} />)}
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,31 +1,6 @@
-import { View, Text, StyleSheet } from 'react-native';
-import { Image } from 'react-native';
-import App from '../../App';
-
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 export default function Card(props) {
-  const cachorros = [
-    {
-      imgcachorro: "./img/bulldog.webp",
-      raça: 'Bulldog', 
-    },
-    {
-      imgcachorro: "./img/pastor-alemão.jpg",
-      raça: 'Pastor Alemão',
-    },
-    {
-      imgcachorro: "./img/pitbull-p.webp",
-      raça: 'Pitbull',
-    },
-    {
-      imgcachorro: "./img/shitzu.jpeg",
-      raça: 'Shitzu',
-    },
-    {
-      imgcachorro: "./img/yorkshire.jpg",
-      raça: 'Yorkshire',
-    },
-  ]
   return (
     <View style={styles.card}>
         <Image style={styles.foto} source={{ uri: props.cachorro.imgcachorro}} />  
